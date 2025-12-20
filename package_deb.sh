1dac62fc0d9ab0af6e4aac7e8133c7ee5b797ee2
zales <o.zalesky@gmail.com>
2025-12-20 14:58:40 +0100

    fix: use -m flag for dpkg-scanpackages to support multi-arch

--- package_deb.sh
+++ package_deb.sh
@@ -2,7 +2,7 @@
 set -e
 
 APP_NAME="sys-ink"
-VERSION="${VERSION:-1.0.4}"
+VERSION="${VERSION:-1.0.5}"
 TARGET_ARCH="${TARGET_ARCH:-amd64}" # debian architecture name: amd64, arm64, armhf
 BINARY_PATH="${BINARY_PATH:-zig-out/bin/sys-ink}"
 
