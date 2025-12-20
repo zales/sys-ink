806a8f227328967d16b83164b10c67f6bcfe8394
zales <o.zalesky@gmail.com>
2025-12-20 15:10:30 +0100

    fix: generate full Release file with hashes and date to silence apt warnings

--- package_deb.sh
+++ package_deb.sh
@@ -2,7 +2,7 @@
 set -e
 
 APP_NAME="sys-ink"
-VERSION="${VERSION:-1.0.6}"
+VERSION="${VERSION:-1.0.7}"
 TARGET_ARCH="${TARGET_ARCH:-amd64}" # debian architecture name: amd64, arm64, armhf
 BINARY_PATH="${BINARY_PATH:-zig-out/bin/sys-ink}"
 
